c8e9153bc62b6106d9949b313934b30fe0f5ef38
Vitaliy Starcev <6asm6deus6@gmail.com>
2023-09-02 15:06:07 +0500

    add: Added start logic and timer action

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,19 +2,33 @@ customElements.define('my-timer', class extends HTMLElement {
     constructor() {
         super()
         this.seconds = this.getAttribute('seconds') || undefined
+        this.minutes = null
+        this.hours = null
+        this.startTrigger = false
+        this.startButton = document.getElementById('startBtn') || undefined
+        this.timerDisplay = document.querySelector('.timer__display') || undefined
     }
     connectedCallback() {
+        // this._shadow = this.attachShadow({mode: 'open'})
+        // const p = document.querySelector('.timer__display')
+        // p.innerHTML = 'Таймер'
+        // this._shadow.append(p)
+        
         this.formattedTime(this.seconds)
+        this.startButton.addEventListener('click', () => {this.startTimer()})
     }
     disconnectedCallback() {
 
     }
+
     static get observedAttributes() {
         return ['seconds']
     }
+
     attributeChangedCallback(name, oldValue, newValue) {
 
     }
+
     formattedTime(seconds) {
         let formattedTime = ''
 
@@ -39,5 +53,39 @@ customElements.define('my-timer', class extends HTMLElement {
         formattedTime += this.seconds.toString().padStart(2, '0')
     
         console.log('Форматированное время:', formattedTime)
+    }
+
+    startTimer() {
+        this.startTrigger = true;
+    
+        const timerInterval = setInterval(() => {
+            if (this.startTrigger) {
+                const formatNumber = (num) => (num < 10 ? `0${num}` : `${num}`);
+                const hoursText = this.hours === 0 ? '' : formatNumber(this.hours);
+                const minutesText = this.minutes === 0 ? '' : formatNumber(this.minutes);
+                const secondsText = this.seconds === 0 ? '' : formatNumber(this.seconds);
+
+                this.timerDisplay.innerText = `${hoursText}${hoursText && minutesText ? ':' : ''}${minutesText}${(hoursText || minutesText) && secondsText ? ':' : ''}${secondsText}`;
+                        
+                if (this.seconds > 0) {
+                    this.seconds -= 1
+                } else {
+                    if (this.minutes > 0) {
+                        this.minutes -= 1
+                        this.seconds = 59
+                    } else {
+                        if (this.hours > 0) {
+                            this.hours -= 1
+                            this.minutes = 59
+                            this.seconds = 59
+                        } else {
+                            this.startTrigger = false
+                            clearInterval(timerInterval)
+                            this.timerDisplay.innerText = 'Время кончилось'
+                        }
+                    }
+                }
+            }
+        }, 1000)
     }
 })
