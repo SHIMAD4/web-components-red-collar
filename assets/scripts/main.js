31a14809813e248622f845e71a8b8437b466f6d5
Vitaliy Starcev <6asm6deus6@gmail.com>
2023-09-03 19:19:47 +0500

    add: Added the ability to enter time via the to-time attribute

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -3,6 +3,7 @@ customElements.define('my-timer', class extends HTMLElement {
     constructor() {
         super()
         // Инициализация секунд, минут и часов
+        this.time = this.getAttribute('to-time') || undefined
         this.seconds = this.getAttribute('seconds') || undefined
         this.minutes = null
         this.hours = null
@@ -37,8 +38,14 @@ customElements.define('my-timer', class extends HTMLElement {
     
     // Инициализация метода при подключении таймера в документ
     connectedCallback() {
+        this._nowTime = new Date().toLocaleTimeString('ru-RU')
+
         // Вызов метода для форматирования секунд в ЧЧ:ММ:СС формат
-        this.formattedTime(this.seconds)
+        if(this.seconds) {
+            this.formattedTime(this.seconds)
+        } else if(this.time) {
+            this.formattedTime(this.time)
+        }
 
         this.startButton.addEventListener('click', () => {                          // Привязка события по клику
             this.startButton.dispatchEvent(this.startTimerEvent)                    // Вызов события по старту таймера
@@ -72,32 +79,59 @@ customElements.define('my-timer', class extends HTMLElement {
 
     // Инициализация геттера для получения всех атрибутов
     static get observedAttributes() {
-        return ['seconds']
+        return ['seconds', 'to-time']
     }
 
-    // Инициализация метода для форматирования секунд в ЧЧ:ММ:СС формат
-    formattedTime(seconds) {
-        let formattedTime = ''                         // Инициализация начальной строки
-
-        if (seconds >= 3600) {
-            this.hours = Math.floor(seconds / 3600)    // Нахождение часов
-            seconds %= 3600                            // Нахождение остатка
+    // Инициализация метода для форматирования времени в ЧЧ:ММ:СС формат
+    formattedTime(time) {
+        let formattedTime = ''                             // Инициализация начальной строки
+        const currentTime = new Date().getTime() / 1000    // Текущее время в секундах
+    
+        if (time.indexOf(':') !== -1) {
+            const timeParts = time.split(':')
+            
+            if (timeParts.length === 3) {
+                const targetTime = new Date().setHours(+timeParts[0], +timeParts[1], +timeParts[2]) / 1000
+                let timeDifference = targetTime - currentTime
+    
+                if (timeDifference >= 0) {
+                    this.hours = Math.floor(timeDifference / 3600)
+                    timeDifference %= 3600
+                    this.minutes = Math.floor(timeDifference / 60)
+                    this.seconds = Math.floor(timeDifference % 60)
+                } else {
+                    // Обработка случая, когда переданное время уже прошло
+                    console.error('Полученное время меньше текущего времени.')
+                    return
+                }
+            } else {
+                // Обработка неверного формата времени
+                console.error('Неверный формат времени. Используйте ЧЧ:ММ:СС.')
+                return
+            }
         } else {
-            this.hours = 0
+            // Обработка времени в секундах
+            if (time >= 3600) {
+                this.hours = Math.floor(time / 3600)
+                time %= 3600
+            } else {
+                this.hours = 0
+            }
+    
+            this.minutes = Math.floor(time / 60)
+            this.seconds = time % 60
         }
     
-        this.minutes = Math.floor(seconds / 60)        // Нахождение минут
-        this.seconds = seconds % 60                    // Нахождение остатка
-    
         if (this.hours > 0) {
-            formattedTime += `${this.hours.toString().padStart(2, '0')}:`      // Вывод на экран кол-во часов
+            formattedTime += `${this.formatNumber(this.hours)}:`
         }
     
         if (this.minutes > 0 || this.hours > 0) {
-            formattedTime += `${this.minutes.toString().padStart(2, '0')}:`    // Вывод на экран кол-во минут
+            formattedTime += `${this.formatNumber(this.minutes)}:`
         }
     
-        formattedTime += this.seconds.toString().padStart(2, '0')              // Вывод на экран кол-во секунд
+        formattedTime += this.formatNumber(this.seconds)
+        this.numberDisplay()
     }
 
     // Инициализация метода для форматирования времени если присутствует 0 перед числом
@@ -157,7 +191,11 @@ customElements.define('my-timer', class extends HTMLElement {
         this.isPaused = false
         this.startTrigger = false
         clearInterval(this.timerInterval)
-        this.formattedTime(this.getAttribute('seconds'))
+        if(this.getAttribute('seconds')) {
+            this.formattedTime(this.seconds)
+        } else if(this.time) {
+            this.formattedTime(this.time)
+        }
         this.numberDisplay()
     }
 })
